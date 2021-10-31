cc5b412d7e9de60ddd75a3b9b91b73074118df37
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-10-31 22:21:51 +0100

    feat: working with arrays

--- dataStructures/arrays/01/main.go
+++ dataStructures/arrays/01/main.go
@@ -4,10 +4,13 @@ import "fmt"
 
 func main() {
 	var x [50]int
+	var y [50]string
 	fmt.Println(x, len(x))
-	for i := 0; i < 50; i++ {
-		x[i] = i
+	for i := 65; i < 100; i++ {
+		x[i-65] = i
+		y[i-65] = string(i)
 	}
 	fmt.Println(x)
+	fmt.Println(y)
 
 }
