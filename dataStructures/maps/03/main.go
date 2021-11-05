0f6c164176303e36526660de8835a8be2e527f86
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-11-05 08:08:39 +0100

    Feat: maps: printing a map in an ordered manner, using a slice to control the order

---
+++ dataStructures/maps/03/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+//printing from a map in an ordered manner using a different slice
+func main() {
+	m := map[int]string{
+		1: "Tony",
+		2: "Ini",
+		3: "Ire",
+		4: "Kunmi",
+	}
+	var keys []int
+	for k := range m {
+		fmt.Println(k)
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Println("Key:", k, "Value:", m[k])
+	}
+}
