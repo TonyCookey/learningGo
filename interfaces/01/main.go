2b96b589db98490a6d756ad00881fb3f5c66f7bf
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-11-10 19:01:58 +0100

    Feat: Interface - using and learning interfaces - updated code to use both circle and square and implement the Shape interface

--- interfaces/01/main.go
+++ interfaces/01/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Square struct {
 	length float64
+}
+type Circle struct {
+	radius float64
 }
 type Shape interface {
 	area() float64
@@ -12,11 +18,16 @@ type Shape interface {
 func (s Square) area() float64 {
 	return s.length * s.length
 }
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
 func info(s Shape) float64 {
 	return s.area()
 }
 
 func main() {
 	sq1 := Square{length: 20}
-	fmt.Println(info(sq1))
+	c1 := Circle{radius: 30}
+	fmt.Println("Square:", info(sq1))
+	fmt.Println("Circle:", info(c1))
 }
