8c6bcb79f320269fbdcdde297eb7517f8f585336
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-11-18 08:18:21 +0100

    Feat: errors: creating custom errors - using struct to implement the error interface and create errors with more context

---
+++ error-handling/08/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+)
+
+func main() {
+	val, err := squareRoot(-36)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Square Root:", val)
+}
+
+func squareRoot(x float64) (float64, error) {
+	if x <= 0 {
+		return 0, fmt.Errorf("computation error: Square Root of a Negative Number, %v", x)
+	}
+	return math.Sqrt(x), nil
+}
---
+++ error-handling/09/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+)
+
+type ErrNegNum struct {
+	errType   string
+	operation string
+	err       error
+}
+
+func (e ErrNegNum) Error() string {
+	return fmt.Sprintln(e.errType, ":", e.operation, "==> ", e.err)
+}
+
+func main() {
+	val, err := squareRoot(-36)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Square Root:", val)
+}
+func squareRoot(x float64) (float64, error) {
+	if x <= 0 {
+		return 0, ErrNegNum{
+			errType:   "Computation Error",
+			operation: "Square Root",
+			err:       fmt.Errorf("square Root of a negative number (%v)", x),
+		}
+	}
+	return math.Sqrt(x), nil
+}
