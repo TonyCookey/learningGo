b94105c920d7b328e0a2396d99322fbb38519d9e
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-11-24 18:54:09 +0100

    Feat: Structs: refresh understanding on composition, promotion, promoted fields and methods, receivers

---
+++ dataStructures/structs/04/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+type student struct {
+	person
+	course string
+	level  string
+}
+
+func (s student) fullName() {
+	fmt.Println(s.firstName, s.lastName)
+}
+func (p person) fullName() {
+	fmt.Println(p.firstName, p.lastName)
+}
+func (s student) identity() {
+	fmt.Println(s.course, s.level)
+}
+func main() {
+	p1 := person{
+		firstName: "Tony",
+		lastName:  "Cookey",
+	}
+	s1 := student{
+		person: person{
+			firstName: "Clara",
+			lastName:  "Johnson",
+		},
+		course: "Humanities",
+		level:  "400",
+	}
+	p1.fullName()
+	s1.fullName()
+	s1.identity()
+}
