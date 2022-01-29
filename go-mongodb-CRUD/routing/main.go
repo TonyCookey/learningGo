8ba112bc6be933d67e5c42c4b5e25f281be75cec
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-01-29 12:48:41 +0100

    importing models into the router file and using the models

--- go-mongodb-CRUD/routing/main.go
+++ go-mongodb-CRUD/routing/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"learningGo/go-mongodb-CRUD/routing/models"
 	"log"
 	"net/http"
 )
@@ -10,6 +12,7 @@ import (
 func main() {
 	router := httprouter.New()
 	router.GET("/", index)
+	router.GET("/profile", profile)
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal(err)
@@ -18,6 +21,20 @@ func main() {
 
 func index(res http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	_, err := fmt.Fprint(res, "Welcome to my Server")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func profile(res http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	user := models.User{
+		FirstName: "Anthony",
+		LastName:  "Iroegbu",
+		Age:       24,
+	}
+	profile, err := json.Marshal(user)
+
+	_, err = fmt.Fprint(res, string(profile))
 	if err != nil {
 		log.Fatal(err)
 	}
